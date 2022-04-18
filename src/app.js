13ac12ba90cf1a50d1578a56f025ae13ed50ff6f
Lucas Gabriel <gabriel.lisboa.141@gmail.com>
2022-04-18 16:54:24 -0300

    conectando servidor na database

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const {json} = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 4001;
+
+app.use(json())
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+
+async function main() {
+    await mongoose.connect(`mongodb+srv://${process.env.MY_USER}:${process.env.MY_PASSWORD}@cluster0.mxpes.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
+    app.listen(port, (req, res) => {
+        console.log(`Server rodando na porta ${port}`);
+    })
+}
+
+main().catch(err => console.log(err));
