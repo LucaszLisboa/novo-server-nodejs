befcbbc72db13ccca23b040e37bbd301517be4f6
LucaszLisboa <gabriel.lisboa.141@gmail.com>
2022-04-25 11:13:49 -0300

    renderizando o servidor

--- src/app.js
+++ src/app.js
@@ -12,6 +12,31 @@ app.use(json())
 app.use(cors());
 app.use('/temperature', temperatures)
 
+app.get('/', (req, res) => {
+    res.status(200).send(
+        `<html>
+            <head></head>
+            <body>
+                <h1> This is a Server Side Render Web page</h1>
+                <div>
+                    <p> Ok, I'm running on port ${port}.
+                </div>
+                <h2>Server RESTful Endpoints:</h2>
+                <ul>
+                    <li>curl -X POST http://localhost:4001/temperature -H 'Content-Type: application/json' -d "{\"temperature\": {value}}"</li>
+                    <li>curl -X GET http://localhost:4001/temperature</li>
+                    <li>curl -X GET http://localhost:4001/temperature/{id}</li>
+                    <li>curl -X PUT http://localhost:4001/temperature/{temperature}?id={id}</li>
+                    <li>curl -X DELETE http://localhost:4001/temperature</li>
+                    <li>curl -X DELETE http://localhost:4001/temperature/query?id={id}</li>
+                </ul>
+            </body>
+        </html>
+        `
+    )
+
+})
+
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
 })
