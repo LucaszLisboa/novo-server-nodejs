872f30561af5da819b3be1bcb2bbccf68903ed70
LucaszLisboa <gabriel.lisboa.141@gmail.com>
2022-04-25 11:09:47 -0300

    adicionando rotas

--- src/app.js
+++ src/app.js
@@ -3,67 +3,16 @@ const { json } = require('body-parser');
 const cors = require('cors');
 // const mongoose = require('mongoose');
 
+const temperatures = require('./routes/temperature')
+
 const app = express();
 const port = 4001;
 
 app.use(json())
 app.use(cors());
+app.use('/temperature', temperatures)
 
-let dummyCount = 0
-let temperatures = [];
-
-app.get('/', (req, res) => {
-    res.status(200).send(temperatures);
-})
-
-app.get('/:id', (req, res) => {
-    const pathId = req.params.id
-    const temperatureObject = temperatures.filter(temperature => temperature.id == pathId);
-    res.status(200).send(temperatureObject)
-})
-
-app.post('/', (req, res) => {
-    const request = req.body
-
-    const temperatureObject = {
-        id: dummyCount += 1,
-        temperature: request.temperature 
-    }
-
-    temperatures.push(temperatureObject)
-    res.status(201).send()
-})
-
-app.delete('/', (req, res) => {
-    temperatures = []
-    res.status(200).send()
-})
-
-app.delete('/query', (req, res) => {
-    const queryId = req.query.id
-
-    filteredList = temperatures.filter(temperature => temperature.id != queryId)
-    temperatures = filteredList
-
-    res.status(200).send()
-})
-
-app.put('/:value', (req, res) => {
-    const pathValue = req.params.value
-    const queryId = req.query.id
-    console.log(`QUERY IS ${queryId} AND PARAMETER IS ${pathValue}`)
-
-    temperatures.map(temperature => {
-        if (temperature.id === queryId) {
-            console.log(`FOUND IS ${queryId} CHANGING VALUE OF OBJECT`)
-            temperature.temperature = pathValue
-        }
-    })
-
-    res.status(200).send()
-})
-
-app.listen(port, (req, res) => {
+app.listen(port, () => {
     console.log(`server running on port ${port}`);
 })
 
