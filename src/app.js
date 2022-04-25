1d225e190fa0869f6b87a88b931acde53516232b
LucaszLisboa <gabriel.lisboa.141@gmail.com>
2022-04-25 11:23:18 -0300

    servindo arquivos HTML

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { json } = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 // const mongoose = require('mongoose');
 
 const temperatures = require('./routes/temperature')
@@ -11,31 +12,14 @@ const port = 4001;
 app.use(json())
 app.use(cors());
 app.use('/temperature', temperatures)
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.status(200).send(
-        `<html>
-            <head></head>
-            <body>
-                <h1> This is a Server Side Render Web page</h1>
-                <div>
-                    <p> Ok, I'm running on port ${port}.
-                </div>
-                <h2>Server RESTful Endpoints:</h2>
-                <ul>
-                    <li>curl -X POST http://localhost:4001/temperature -H 'Content-Type: application/json' -d "{\"temperature\": {value}}"</li>
-                    <li>curl -X GET http://localhost:4001/temperature</li>
-                    <li>curl -X GET http://localhost:4001/temperature/{id}</li>
-                    <li>curl -X PUT http://localhost:4001/temperature/{temperature}?id={id}</li>
-                    <li>curl -X DELETE http://localhost:4001/temperature</li>
-                    <li>curl -X DELETE http://localhost:4001/temperature/query?id={id}</li>
-                </ul>
-            </body>
-        </html>
-        `
-    )
+    res.status(200).sendFile(path.join(__dirname, 'views', "index.html"))
 
 })
+
+app.use((req, res) => res.status(404).sendFile(path.join(__dirname, 'views', "404.html")))
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
