c22e94a7451965433b0e405c0790725a99e951f9
LucaszLisboa <gabriel.lisboa.141@gmail.com>
2022-04-25 11:34:34 -0300

    database conection

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { json } = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-// const mongoose = require('mongoose');
 
 const temperatures = require('./routes/temperature')
 
@@ -24,11 +24,11 @@ app.listen(port, () => {
     console.log(`server running on port ${port}`);
 })
 
-// async function main() {
-//     await mongoose.connect(`mongodb+srv://${process.env.MY_USER}:${process.env.MY_PASSWORD}@cluster0.mxpes.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
-//     app.listen(port, (req, res) => {
-//         console.log(`Server rodando na porta ${port}`);
-//     })
-// }
+async function main() {
+    await mongoose.connect(`mongodb+srv://${process.env.MY_USER}:${process.env.MY_PASSWORD}@cluster0.mxpes.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
+    app.listen(port, (req, res) => {
+        console.log(`Server rodando na porta ${port}`);
+    })
+}
 
-// main().catch(err => console.log(err));
+main().catch(err => console.log(err));
