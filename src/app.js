21091719e068f0d8fb25dbc4be4dbd2332bc712d
LucaszLisboa <gabriel.lisboa.141@gmail.com>
2022-04-25 10:54:13 -0300

    mudando valores dos objetos com PUT

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const {json} = require('body-parser');
+const { json } = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 const app = express();
 const port = 4001;
@@ -9,32 +9,69 @@ const port = 4001;
 app.use(json())
 app.use(cors());
 
+let dummyCount = 0
 let temperatures = [];
 
 app.get('/', (req, res) => {
-    res.status(200).send(`${temperatures}\n`);
+    res.status(200).send(temperatures);
+})
+
+app.get('/:id', (req, res) => {
+    const pathId = req.params.id
+    const temperatureObject = temperatures.filter(temperature => temperature.id == pathId);
+    res.status(200).send(temperatureObject)
 })
 
 app.post('/', (req, res) => {
     const request = req.body
-    console.log(request.temperature)
-    res.status(201).send('POST its working\n')
-})
 
-app.put('/', (req, res) => {
-    res.send('PUT its working\n');
+    const temperatureObject = {
+        id: dummyCount += 1,
+        temperature: request.temperature 
+    }
+
+    temperatures.push(temperatureObject)
+    res.status(201).send()
 })
 
 app.delete('/', (req, res) => {
-    temperatures = [];
-    res.status(200).send('DELETE its working\n');
+    temperatures = []
+    res.status(200).send()
 })
 
-async function main() {
-    await mongoose.connect(`mongodb+srv://${process.env.MY_USER}:${process.env.MY_PASSWORD}@cluster0.mxpes.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
-    app.listen(port, (req, res) => {
-        console.log(`Server rodando na porta ${port}`);
-    })
-}
+app.delete('/query', (req, res) => {
+    const queryId = req.query.id
 
-main().catch(err => console.log(err));
+    filteredList = temperatures.filter(temperature => temperature.id != queryId)
+    temperatures = filteredList
+
+    res.status(200).send()
+})
+
+app.put('/:value', (req, res) => {
+    const pathValue = req.params.value
+    const queryId = req.query.id
+    console.log(`QUERY IS ${queryId} AND PARAMETER IS ${pathValue}`)
+
+    temperatures.map(temperature => {
+        if (temperature.id === queryId) {
+            console.log(`FOUND IS ${queryId} CHANGING VALUE OF OBJECT`)
+            temperature.temperature = pathValue
+        }
+    })
+
+    res.status(200).send()
+})
+
+app.listen(port, (req, res) => {
+    console.log(`server running on port ${port}`);
+})
+
+// async function main() {
+//     await mongoose.connect(`mongodb+srv://${process.env.MY_USER}:${process.env.MY_PASSWORD}@cluster0.mxpes.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
+//     app.listen(port, (req, res) => {
+//         console.log(`Server rodando na porta ${port}`);
+//     })
+// }
+
+// main().catch(err => console.log(err));
